ee9f6edfb2efc89a717bdf923c8399cc7405042c
SaGe <m.ozturkes@gmail.com>
2024-02-28 18:23:17 +0300

    Initial Commit

---
+++ Programatic Project/HomeVC.swift
@@ -0,0 +1,20 @@
+//
+//  HomeVC.swift
+//  Programatic Project
+//
+//  Created by mücahit öztürk on 28.02.2024.
+//
+
+import UIKit
+
+class HomeVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        view.backgroundColor = .yellow
+    }
+    
+
+
+}
